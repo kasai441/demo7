db2747ec76d4e6df86140fc70aa7fb5ec6a97327
kasai441 <kasai441441@gmail.com>
2022-03-01 14:14:07 +0900

    Vue.jsとBabelのプラグインが機能していることを確認

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+import { createApp } from 'vue';
+import Hello from './hello.vue';
+
+console.log("lehoo wordl")
+
+document.addEventListener('DOMContentLoaded', () => {
+    createApp(Hello).mount('#app');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path    = require("path")
 const webpack = require("webpack")
+const { VueLoaderPlugin } = require('vue-loader');
+const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
+  mode,
   entry: {
     application: "./app/javascript/application.js"
   },
@@ -12,9 +13,31 @@ module.exports = {
     sourceMapFilename: "[name].js.map",
     path: path.resolve(__dirname, "app/assets/builds"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(vue)$/,
+        exclude: /node_modules/,
+        use: ['vue-loader'],
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
-    })
-  ]
+    }),
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false
+    }),
+    new VueLoaderPlugin()
+  ],
+  optimization: {
+    moduleIds: 'hashed',
+  }
 }
